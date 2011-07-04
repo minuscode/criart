4ea6bbd0ae23905639ea00405c10f5ea4c873dfe
Miguel Marinhas <miguel.marinhas@gmail.com>
2011-07-04 20:43:34 +0100

    initial release criart app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Criart::Application.config.secret_token = '2c13450184e678e5d7eca11f5e501fc1a4a1d264aa27cc537c761f9c35235f265fb201075204d8612ad5e054b2a269e6c6b8ef1bac7e408a9a1bada02acb6633'
