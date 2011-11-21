ac1189e4d6e7528b636390613540d1a5760f7b44
João Araújo <tonas14@gmail.com>
2011-11-21 14:36:18 +0000

    Cleanup do projecto

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Criart::Application.config.session_store :cookie_store, :key => '_criart_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Criart::Application.config.session_store :active_record_store
