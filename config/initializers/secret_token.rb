fb2c53b9ff4b54c6c53469002890e75648583840
João Araújo <tonas14@gmail.com>
2011-11-21 14:52:51 +0000

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Criart::Application.config.secret_token = '7efea55ebaa889e0e8f931595b02cf9d4bea6764dac9058814a8f2731ffcb879d50e4500ba19de13e3532e1d0d721637f3676a3461dbe6231d987fc7c4f559af'
