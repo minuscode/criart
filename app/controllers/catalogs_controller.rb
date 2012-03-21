bfb0d8caaedcb7e1408b2ebc5ba52069b08a5861
João Araújo <tonas14@gmail.com>
2012-03-21 23:41:08 +0000

    Catalogs Fist Draft

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,2 +1,9 @@
 class CatalogsController < InheritedResources::Base
+  def index
+    @catalogs = Catalog.all
+  end
+  def show
+    @catalog = Catalog.find(params[:id])
+    @products = @catalog.products
+  end  
 end
